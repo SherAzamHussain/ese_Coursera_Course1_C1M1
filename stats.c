4b7fa6f68c8d170309e41e3915ebad5d3ad8cbea
Azam <azamhussain.25@iiee.edu.pk>
2018-06-28 23:49:07 -0600

    Adding code outline and documentation for C1M1 Assesment

--- stats.c
+++ stats.c
@@ -35,7 +35,7 @@
 /* Size of the Data Set */
 #define SIZE (40)
 
-void main() {
+int main(int argc, char *argv[]) {
 
   unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
                               114, 88,   45,  76, 123,  87,  25,  23,
@@ -43,42 +43,158 @@ void main() {
                               201,   6,  12,  60,   8,   2,   5,  67,
                                 7,  87, 250, 230,  99,   3, 100,  90};
 
-  /* Other Variable Declarations Go Here */
-  /* Statistics and Printing Functions Go Here */
+  double mean;
+  double median;
+  unsigned char max_element;
+  unsigned char min_element;
+  unsigned char *sorted_array;
+  
+  
+  mean = find_mean(test, SIZE);
+  median = find_median(test, SIZE);
+  max_element = find_maximum(test, SIZE);
+  min_element = find_minimum(test, SIZE);
+  sorted_array = sort_array(test, SIZE);
+  
+  print_array(test, SIZE);
+  printf("\nsorted array is:\n");
+  int i;
+  for(i = 0; i < SIZE; i++){
+    
+    printf("%d\t", sorted_array[i]);
+  
+  }
+  printf("\nmean of array is: %.2f \n\n", mean);
+  printf("median of array is: %.2f \n\n", median);
+  printf("maximum element of array is: %d \n\n", max_element);
+  printf("minimum element of array is: %d \n\n", min_element);
+  print_statistics(test, SIZE);
+ 
 
+  return 0;
 }
 
 /** functiion definitioins**/
 
-int find_median(char myArray[], int length){
+/* definition of print_statistics() function*/
+
+void print_statistics(unsigned char myArray[], int length){
+      
+     double mean;
+     double median;
+     unsigned char max_element;
+     unsigned char min_element;
+     unsigned char *sorted_array;
+     
+     sorted_array = sort_array(myArray, length);
+     mean = find_mean(myArray, length);
+     median = find_median(myArray, length);
+     max_element = sorted_array[0];
+     min_element = sorted_array[length - 1];
+     
+     
+     printf("statistics of array are:\n");
+     printf("mean: %.2f  median: %.2f  maximum: %d  minimum: %d \n", mean, median, max_element, min_element);
 
 
 }
 
-float find_mean(char myArray[], int length){
+/* definition of find_median() function*/
+
+double find_median(unsigned char myArray[], int length){
+      
+     int mid_index1;
+     int mid_index2;
+     unsigned char *sorted_array;
+     unsigned char mid_element1;
+     unsigned char mid_element2;
+     double median;
+     
+     mid_index1 = SIZE / 2;
+     mid_index2 = mid_index1 + 1;
+     sorted_array = sort_array(myArray, length);
+     mid_element1 = sorted_array[mid_index1];
+     mid_element2 = sorted_array[mid_index2];
+     
+     median = (mid_element1 + mid_element2) / 2;
+     
+     return median;
 
 }
 
+/* definition of find_mean() function*/
 
-int find_maximum(char myArray[], int length){
+double find_mean(unsigned char myArray[], int length){
+     int i;
+     double mean;
+     double sum;
+     sum = 0;
+     for(i = 0; i < SIZE; i++){
+     sum += myArray[i];
 
-}
-
-int find_minimum(char myArray[], int length){
-
-}
-
-int *sort_array(char myArray[], int length){
-
-
-}
-
-struct print_statistics(char myArray[], int length){
-
+     }
+     
+     mean = sum / SIZE;
+     return mean;
 
 }
 
-void print_array(char myArray[], int length){
+/* definition of find_maximum() function*/
+
+unsigned char find_maximum(unsigned char myArray[], int length){
+       unsigned char *sorted_array;
+       unsigned char max_element;
+       /* sort the array in decending order*/
+       sorted_array = sort_array(myArray, SIZE); 
+       max_element = sorted_array[0];
+       
+       return max_element;
+}
+
+/* definition of find_minimum() function*/
+
+unsigned char find_minimum(unsigned char myArray[], int length){
+       unsigned char *sorted_array;
+       unsigned char min_element;
+       /* sort the array in decending order*/
+       sorted_array = sort_array(myArray, SIZE); 
+       min_element = sorted_array[SIZE - 1];
+       
+       return min_element;
+
+}
+
+/* definition of sort_array() function*/
+
+unsigned char *sort_array(unsigned char myArray[], int length){
+      int i;
+      int j;
+      int temp;
+      for(i = 0; i < length; i++){
+
+         for(j = i+1; j < length; j++){
+
+            if(myArray[i] < myArray[j]){
+
+            temp = myArray[i];
+            myArray[i] = myArray[j];
+            myArray[j] = temp;
+            }     
+         }
+      }
+
+      return myArray;
+}
+
+/* definition of print_array() function*/
+
+void print_array(unsigned char myArray[], int length){
+    int i;
+    printf("The contents of array are:\n");
+    for(i = 0; i < SIZE; i++){
+    printf("%d\t", myArray[i]);
+      
+    }
 
 }
 
--- stats.h
+++ stats.h
@@ -11,11 +11,11 @@
 #ifndef __STATS_H__
 #define __STATS_H__
 
-/**
+/**  declaration of find_median() function
  * @brief calculates the median of array
  *
- * This function takes as input an array of data and array length as inputs. The function will 
- * compute the median of input array and return the median as output integer
+ * This function takes an array of data and array length as inputs. The function will 
+ * compute the median of input array and return the median as output.
 
 
  * @param myArray[] array of data
@@ -24,13 +24,13 @@
  *
  */
 
-int find_median(char myArray[], int length);
+double find_median(unsigned char myArray[], int length);
 
-/**
+/**  declaration of find_mean() function
  * @brief calculates the mean of array
  *
  * This function takes as input an array of data and array length as inputs. The function will 
- * compute the mean of input array and return the mean as output float
+ * compute the mean of input array and return the mean as output.
 
 
  * @param myArray[] array of data
@@ -39,10 +39,11 @@ int find_median(char myArray[], int length);
  *
  */
 
-float find_mean(char myArray[], int length);
+double find_mean(unsigned char myArray[], int length);
 
 
-/**
+/**    declaration of find_maximum() function
+ *
  * @brief calculates the maximum of array
  *
  * This function takes as input an array of data and array length as inputs. The function will 
@@ -55,10 +56,11 @@ float find_mean(char myArray[], int length);
  *
  */
 
-int find_maximum(char myArray[], int length);
+unsigned char find_maximum(unsigned char myArray[], int length);
 
 
-/**
+/**  declaration of find_minimum() function
+ * 
  * @brief finds the minimum element of array
  *
  * This function takes  an array of data and array length as inputs. The function will 
@@ -70,10 +72,11 @@ int find_maximum(char myArray[], int length);
  *
  */
 
-int find_minimum(char myArray[], int length);
+unsigned char find_minimum(unsigned char myArray[], int length);
 
 
-/**
+/**   declaration of sort_array() function
+ *
  * @brief sorts the array from largest to smallest element.
  *
  * This function takes  an array of data and array length as inputs. The function will 
@@ -86,25 +89,27 @@ int find_minimum(char myArray[], int length);
  *
  */
 
-int *sort_array(char myArray[], int length);
+unsigned char *sort_array(unsigned char myArray[], int length);
 
 
-/**
+/**   declaration of print_statistics() function
+ *
  * @brief prints the statistics of array.
  *
  * This function takes  an array of data and array length as inputs. The function will 
- * return the statistics of array including, median, mean, minimum and maximum.
+ * print the statistics of array including, median, mean, minimum and maximum.
 
  * @param myArray[] array of data
  * @param length    length of input array
- * @return a struct which contains, mean, median, minimum and maximum value of array.
+ * 
  *
  */
 
-struct print_statistics(char myArray[], int length);
+void print_statistics(unsigned char myArray[], int length);
 
 
-/**
+/**   declaration of print_array() function
+ *
  * @brief prints the contents of array.
  *
  * This function takes  an array of data and array length as inputs. The function will 
@@ -115,7 +120,7 @@ struct print_statistics(char myArray[], int length);
  * 
  */
 
-void print_array(char myArray[], int length);
+void print_array(unsigned char myArray[], int length);
 
 
 
